f9c225eb5352574578fcb487efa945a4d087c366
Gregor Ziegltrum <gregor.ziegltrum@tngtech.com>
2023-09-15 15:15:08 +0200

    change logic to 5

--- coinSelectorRead/main.py
+++ coinSelectorRead/main.py
@@ -3,7 +3,7 @@ import time
 import serial
 from pynput.keyboard import Controller
 
-ser = serial.Serial('COM5', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 # read the serial connection
 keyboard = Controller()
 c = 0
@@ -11,6 +11,11 @@ while True:
     byt = ser.readline()
     counter = int(byt.decode())
     for _ in range(counter):
+        c += 1
+        if c < 5:
+            continue
+        c = 0
         keyboard.press('a')
-        time.sleep(0.01)
+        time.sleep(0.02)
         keyboard.release('a')
+        time.sleep(0.02)
